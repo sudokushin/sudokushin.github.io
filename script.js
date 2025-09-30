699872d07e01a8b797f565cef9e1eda46f13b43c
Shin <133396580+sudokushin@users.noreply.github.com>
2025-09-30 14:14:01 +0200

    redesign to white color

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ cards.forEach(card => {
   card.addEventListener('mouseenter', () => {
     cards.forEach(c => {
       if(c !== card) c.style.transform = 'scale(0.9)';
-      else c.style.transform = 'scale(1.2)';
+      else {c.style.transform = 'scale(1.2)'; c.style.boxShadow = "0 0 10px white";}
     });
   });
 
   card.addEventListener('mouseleave', () => {
     cards.forEach(c => c.style.transform = 'scale(1)');
+    cards.forEach(c => c.style.boxShadow = '0 0 0');
   });
 });
